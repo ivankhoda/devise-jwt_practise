eb9ee8f3382674e66fae1468dee7c8ae633ded73
Ivan Khoda <ivankhoda01@gmail.com>
2022-09-06 22:48:02 +0300

    Add migrations

--- db/migrate/20220905193528_create_organisations.rb
+++ db/migrate/20220905193528_create_organisations.rb
@@ -1,7 +1,17 @@
 class CreateOrganisations < ActiveRecord::Migration[7.0]
   def change
     create_table :organisations do |t|
+      t.string :name, null: false
+      t.integer :code
+      t.string :website
+      t.string :email
+      t.datetime :established_at
+      t.boolean :is_active
       t.timestamps
     end
+    add_index :organisations, :id
+    add_reference :industries, :industry, foreign_key: true
+    add_reference :regions, :region, foreign_key: true
+    add_reference :items, :item, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_04_180228) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_193734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "industries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
     t.datetime "exp", null: false
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
+  create_table "organisations", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "code"
+    t.string "website"
+    t.string "email"
+    t.datetime "established_at"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["id"], name: "index_organisations_on_id"
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
